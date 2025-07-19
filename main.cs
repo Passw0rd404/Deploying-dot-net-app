6b509c2860b45cdf7829591df70fbc7717044927
Pa$$w0rd <a26263842z@gmail.com>
2025-07-19 15:42:09 +0300

    v3

--- main.cs
+++ main.cs
@@ -9,12 +9,12 @@ class Program
     {
         Console.WriteLine("httplistener");
         String[] s3_facts = new String[] {
-              "Scale storage resources to meet fluctuating needs with 99.999999999% (11 9s) of data durability.",
-        "Store data across Amazon S3 storage classes to reduce costs without upfront investment or hardware refresh cycles.",
-        "Protect your data with unmatched security, compliance, and audit capabilities.",
-        "Easily manage data at any scale with robust access controls, flexible replication tools, and organization-wide visibility.",
-        "Run big data analytics, artificial intelligence (AI), machine learning (ML), and high performance computing (HPC) applications.",
-        "Meet Recovery Time Objectives (RTO), Recovery Point Objectives (RPO), and compliance requirements with S3’s robust replication features."
+              "v2 Scale storage resources to meet fluctuating needs with 99.999999999% (11 9s) of data durability.",
+        "v2 Store data across Amazon S3 storage classes to reduce costs without upfront investment or hardware refresh cycles.",
+        "v2 Protect your data with unmatched security, compliance, and audit capabilities.",
+        "v2 Easily manage data at any scale with robust access controls, flexible replication tools, and organization-wide visibility.",
+        "v2 Run big data analytics, artificial intelligence (AI), machine learning (ML), and high performance computing (HPC) applications.",
+        "v2 Meet Recovery Time Objectives (RTO), Recovery Point Objectives (RPO), and compliance requirements with S3’s robust replication features."
         };
 
         var listener = new HttpListener();

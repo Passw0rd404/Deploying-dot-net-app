0373f68f245ef8e5ca47a5293c1cf15e305fca35
Pa$$w0rd <a26263842z@gmail.com>
2025-07-21 16:13:06 +0300

    v4

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
+echo "Installing dotnet service"
 
-DLL_PATH="/home/ubuntu/srv-02/bin/Release/net8.0/linux-x64/publish/S3FactsServer.dll"
-
-cat > /etc/systemd/system/srv-02.service << EOL
+# Create systemd service
+sudo cat /etc/systemd/system/srv-02.service <<EOL
 [Unit]
 Description=Dotnet S3 info service
 After=network.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/dotnet $DLL_PATH
+ExecStart=/usr/bin/dotnet /srv-02/bin/Release/net8.0/linux-x64/publish/srv02.dll
 Restart=always
 RestartSec=5
 User=ubuntu
-WorkingDirectory=/home/ubuntu/srv-02/bin/Release/net8.0/linux-x64/publish
+Environment=ASPNETCORE_ENVIRONMENT=Production
+WorkingDirectory=/srv-02/bin/Release/net8.0/linux-x64/publish
 
 [Install]
 WantedBy=multi-user.target
 EOL
 
 systemctl daemon-reload
-systemctl enable srv-02
-systemctl start srv-02
